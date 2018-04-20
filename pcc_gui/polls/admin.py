0a8c5900dc91642c7c470d4c30850910f3a6d460
Rick Cottle <richard.cottle@honeywell.com>
2018-04-20 14:03:28 -0700

    Working through polls tutorial. Finished polls tutorial.

--- pcc_gui/polls/admin.py
+++ pcc_gui/polls/admin.py
@@ -2,8 +2,23 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question information', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
