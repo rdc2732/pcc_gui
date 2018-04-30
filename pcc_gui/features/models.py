f48416988bd0585e45c6ffb6d3dc2fbf32633bc0
Rick Cottle <richard.cottle@honeywell.com>
2018-04-30 15:59:18 -0700

    Starting features app.

--- pcc_gui/features/models.py
+++ pcc_gui/features/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Feature(models.Model):
+    group_name = models.CharField(max_length=100, blank=True)                # FMM page
+    function_name = models.CharField(max_length=100, blank=True)             # FMM major function group
+    feature_name = models.CharField(max_length=50, blank=True)               # FMM feature name
+    selection_name = models.CharField(max_length=100, blank=True)            # FMM keyword selection
+    choice_type = models.CharField(max_length=15, blank=True)                # FMM valid values are 'selection' or 'option'
+    option_min = models.IntegerField(blank=True, null=True)                  # FMM range of options minimum
+    option_max = models.IntegerField(blank=True, null=True)                  # FMM range of options maximum
+    processed = models.NullBooleanField()                   # FMM.txt row was processed = True; else created from dependency
+
+class Dependency(models.Model):
+    feature = models.ForeignKey(Feature)                # list of features that the related feature depends upon
+
+class Dependent(models.Model):
+    feature = models.ForeignKey(Feature)                # list of features that are dependent upon the related
+
--- pcc_gui/pcc_gui/urls.py
+++ pcc_gui/pcc_gui/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^features/', include('features.urls')),
     url(r'^admin/', admin.site.urls),
 ]
