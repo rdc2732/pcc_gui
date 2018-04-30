a2c248a4da5dc5f5dc5bf988e6b4c8b9f4b440b0
Rick Cottle <richard.cottle@honeywell.com>
2018-04-30 08:19:46 -0700

    Starting features app.

---
+++ pcc_gui/FMM GUI.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1524860534088" ID="ID_682820029" MODIFIED="1524861981873" TEXT="FMM GUI">
+<node CREATED="1524860862011" ID="ID_1770317875" MODIFIED="1524860868127" POSITION="right" TEXT="User Interface">
+<node CREATED="1524860874125" ID="ID_950626302" MODIFIED="1524860900647" TEXT="One tab for each &apos;area&apos;"/>
+<node CREATED="1524860901122" ID="ID_377004758" MODIFIED="1524860933502" TEXT="Rows and columns for features"/>
+<node CREATED="1524860925218" ID="ID_1043726441" MODIFIED="1524860944968" TEXT="Radio buttons to select things"/>
+</node>
+<node CREATED="1524860962933" ID="ID_1690090478" MODIFIED="1524860974883" POSITION="right" TEXT="Feature choices">
+<node CREATED="1524860976966" ID="ID_32160697" MODIFIED="1524864102962" TEXT="Selection, at least one selectable will be enabled. "/>
+<node CREATED="1524860980047" ID="ID_1176646005" MODIFIED="1524864089816" TEXT="If Option, zero or more options can be enabled"/>
+</node>
+<node CREATED="1524860986560" ID="ID_681087940" MODIFIED="1524862700428" POSITION="left" TEXT="Dependencies">
+<node CREATED="1524861985422" ID="ID_1546933461" MODIFIED="1524862031365" TEXT="State of one feature drives behavior of another"/>
+<node CREATED="1524863403710" ID="ID_1971398195" MODIFIED="1524863938978" TEXT="All dependencies must be met in order for that keyword to become enabled in the GUI"/>
+</node>
+<node CREATED="1524860998159" ID="ID_1351548148" MODIFIED="1524861001498" POSITION="left" TEXT="Operationally">
+<node CREATED="1524861002910" ID="ID_10548601" MODIFIED="1524861014688" TEXT="User interface dynamically created"/>
+<node CREATED="1524861019782" ID="ID_123053671" MODIFIED="1524861039564" TEXT="Optional items dynamically change"/>
+</node>
+<node CREATED="1524861600215" ID="ID_969177883" MODIFIED="1524861606673" POSITION="right" TEXT="FMM.txt fields">
+<node CREATED="1524861613007" ID="ID_1715177724" MODIFIED="1524863473224" TEXT="page: Defines location of FM selection/option on GUI"/>
+<node CREATED="1524861617960" ID="ID_1336513844" MODIFIED="1524863584296" TEXT="function: Major Function descriptor for FM user interface"/>
+<node CREATED="1524861620947" ID="ID_645243451" MODIFIED="1524863627399" TEXT="option: Choices within each Major Function for FM user interface"/>
+<node CREATED="1524861623950" ID="ID_1627099061" MODIFIED="1524863639006" TEXT="fms: Unique keyword for each Selectable Option. Keywords link the FMM and DOORS module requirements "/>
+<node CREATED="1524861627920" ID="ID_1462814928" MODIFIED="1524863989569" TEXT="dependency: Defines fms dependencies for FM Selection enable. "/>
+<node CREATED="1524861632374" ID="ID_1461642572" MODIFIED="1524864040590" TEXT="type: The type of feature choice: Selection or Option"/>
+<node CREATED="1524861640229" ID="ID_164506024" MODIFIED="1524863703746" TEXT="min: Minimum number of Selectable Options within Function"/>
+<node CREATED="1524861642858" ID="ID_1171585490" MODIFIED="1524863717006" TEXT="max: Maximum number of Selectable Options within Function"/>
+</node>
+</node>
+</map>
